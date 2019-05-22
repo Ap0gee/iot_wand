f2c5e6db12e560423ced4e3b6b7589f941a0cd49
Sjshovan <Sjshovan@Gmail.com>
2019-05-22 19:59:27 -0400

    no message

--- iot_wand/server/server.py
+++ iot_wand/server/server.py
@@ -193,8 +193,17 @@ class ProfileSelectState(ServerState):
         self.speed_clicks = 0
         self.quaternion_state = _h.Quaternion(0, 0, 0, 0)
 
-        #loop = asyncio.get_event_loop()
-        #loop.run_until_complete(self.run_async_tasks)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.run_async_tasks)
+
+    async def run_async_tasks(self):
+        await asyncio.gather(
+            self.cycle_profiles()
+        )
+
+    async def cycle_profiles(self):
+        await asyncio.sleep(1)
+        print('test')
 
     def on_quaternion(self, interface, x, y, z, w):
         self.quaternion_state.x = x
