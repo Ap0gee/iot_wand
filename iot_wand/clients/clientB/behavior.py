f633fd0423c5edcfbc9e64d5a03191dd68b7e0e7
Sjshovan <Sjshovan@Gmail.com>
2019-05-23 18:26:53 -0400

    no message

--- iot_wand/clients/clientB/behavior.py
+++ iot_wand/clients/clientB/behavior.py
@@ -1,8 +1,15 @@
 from iot_wand import helpers as _h
 import settings as _s
+from pymouse import PyMouse
+
+_m = PyMouse()
+x_dim, y_dim = _m.screen_size()
 
 def on_spell(gesture, spell):
     print(spell)
 
 def on_quaternion(x, y, z, w):
-    print(x, y, x, w)
+    x_pos = x_dim * ((x * 4 + 1000) / 2000)
+    y_pos = y_dim * (1.0 - (y * 4 + 1000) / 2000)
+    # Move the mouse
+    _m.move(int(round(x_pos)), int(round(y_pos)))
