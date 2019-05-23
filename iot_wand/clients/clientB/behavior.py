f9a0a5926a767f2b354916022f47454be0010bde
Sjshovan <Sjshovan@Gmail.com>
2019-05-23 19:17:57 -0400

    no message

--- iot_wand/clients/clientB/behavior.py
+++ iot_wand/clients/clientB/behavior.py
@@ -3,7 +3,7 @@ import settings as _s
 import time
 import win32api
 
-current = win32api.GetCursorPos()
+#current = win32api.GetCursorPos()
 x_dim = win32api.GetSystemMetrics(0)
 y_dim = win32api.GetSystemMetrics(1)
 
@@ -11,8 +11,9 @@ def on_spell(gesture, spell):
     print(spell)
 
 def on_quaternion(x, y, z, w):
-    x_pos = x_dim * ((x * 4 + 1000) / 2000)
-    y_pos = y_dim * (1.0 - (y * 4 + 1000) / 2000)
-    print(int(round(x_pos)),int(round(y_pos)))
+    x_pos = int(round(x_dim * ((int(x) * 4 + 1000) / 2000)))
+    y_pos = int(round(y_dim * (1 - (int(y) * 4 + 1000) / 2000)))
 
-    win32api.SetCursorPos((int(round(x_pos)),int(round(y_pos))))
+    print(x_pos, y_pos)
+
+    win32api.SetCursorPos((x_pos, y_pos))
