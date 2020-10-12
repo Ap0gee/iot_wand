222e80146d040ed14e1cb9057dfb601fe3d2b218
Sjshovan <SJSHOVAN@GMAIL.COM>
2020-10-12 04:10:32 -0400

    asd

--- tests/test_hue.py
+++ tests/test_hue.py
@@ -1,31 +1,36 @@
 from iot_wand.btle_scanners import HueScanner
 from iot_wand.btle_inerfaces import HueInterface
 
+lights = []
+
 def main():
-    lights = []
+    global lights
     debug = True
     hue_scanner = HueScanner(debug=debug)
     if not len(lights):
-        lamps = [
+        lights = [
             HueInterface(device, debug=debug).connect()
-            for device in hue_scanner.scan()
+            for device in hue_scanner.scan(discovery_callback=callback)
         ]
 
-        for lamp in lamps:
-            try:
-                services = lamp.getServices()
-                for service in services:
-                    print('----------service--------')
-                    print(service.uuid)
-                    print('------------------------')
-                    for char in service.getCharacteristics():
-                        print(char.uuid)
-                        print("HANDLE=" + str(char.getHandle()))
-                        print(char.propertiesToString())
-                       
-                    print('------------------------', end="\r\n\r\n")
-            except Exception as e:
-                print(e)
+
+
+def callback(devices):
+  for light in lights:
+        try:
+            services = light.getServices()
+            for service in services:
+                print('----------service--------')
+                print(service.uuid)
+                print('------------------------')
+                for char in service.getCharacteristics():
+                    print(char.uuid)
+                    print("HANDLE=" + str(char.getHandle()))
+                    print(char.propertiesToString())
+
+                print('------------------------', end="\r\n\r\n")
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
