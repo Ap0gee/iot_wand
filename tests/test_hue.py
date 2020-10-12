a35fb1fb1ac9124780d0c5792e93a8b819072afa
Sjshovan <SJSHOVAN@GMAIL.COM>
2020-10-12 04:04:21 -0400

    no message

--- tests/test_hue.py
+++ tests/test_hue.py
@@ -13,14 +13,18 @@ def main():
 
         for lamp in lamps:
             try:
-                _hue_service = lamp.getServiceByUUID('932c32bd-0000-47a2-835a-a8d455b859dd')
-                for char in _hue_service.getCharacteristics():
-                    print(_hue_service.getCharacteristics(char.uuid)[0].getHandle())
-                #handle = _hue_service.getCharacteristics('932c32bd-0003-47a2-835a-a8d455b859dd')[0]
-                #print("Write Start")
-                #print(handle.getHandle())
-                lamp.writeCharacteristic(38, bytes(00))
-                print("write complete")
+                services = lamp.getServices()
+                for service in services:
+                    print(service.uuid)
+                    print('------------------------')
+                    for char in service.getCharacteristics():
+                        print(char.uuid)
+                        print("HANDLE=" + str(char.getHandle()))
+                        print(char.propertiesToString())
+                        print("VAL=" + char.read().decode('utf-8'))
+
+
+                    print('------------------------', end="\r\n\r\n")
             except Exception as e:
                 print(e)
 
