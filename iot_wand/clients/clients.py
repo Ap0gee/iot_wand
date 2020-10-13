30a5d36ee28e075545e4912e102c08ccf2d94edd
Sjshovan <SJSHOVAN@GMAIL.COM>
2020-10-12 21:07:51 -0400

    asd

--- iot_wand/clients/clients.py
+++ iot_wand/clients/clients.py
@@ -3,7 +3,6 @@ import subprocess
 import iot_wand.clients.settings as _s
 from iot_wand import helpers as _h
 import platform
-import threading
 
 def main(dir_top):
     config = _h.yaml_read(_s.PATH_CONFIG)
@@ -15,19 +14,13 @@ def main(dir_top):
                 path_client = entry.path
                 system = platform.system()
                 python = 'python'
-                title = os.path.basename(path_client)
                 terminal_cmd = 'start cmd /K'
                 print(system)
                 if system == 'Linux':
                     terminal_cmd = 'lxterminal -e'
                     python = '$py3'
                 cmd = "%s %s %s %s" % (terminal_cmd, python, os.path.join(path_client, 'client.py'), dir_top)
-                print(cmd)
-                cmd_thread = threading.Thread(target=open_new_terminal, args=(cmd,))
-                cmd_thread.start()
+                subprocess.call(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
     else:
         print("No active clients.")
-
-def open_new_terminal(cmd):
-    subprocess.call(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
