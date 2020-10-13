57e506b5d8f4af4290a397cba99c0887ad8e5171
Sjshovan <SJSHOVAN@GMAIL.COM>
2020-10-13 04:23:56 -0400

    no message

--- iot_wand/clients/all_hues/behavior.py
+++ iot_wand/clients/all_hues/behavior.py
@@ -125,6 +125,6 @@ def on_quaternion(x, y, z, w):
     global lights_manager
 
     if lights_manager.state == LIGHTS_STATES.BRIGHTNESS.value:
-        print(math.floor(abs(w/100)))
+        print(abs(w))
         #print(lights_manager.brightness)
         #lights_manager.brightness = w
