839f1e79b36dae580ab7f40b1b1da7be84a46e93
Sjshovan <SJSHOVAN@GMAIL.COM>
2020-10-12 22:53:40 -0400

    no message

--- iot_wand/server/server.py
+++ iot_wand/server/server.py
@@ -20,7 +20,7 @@ def main():
 
 class AsyncServerStateManager:
     def __init__(self, mqtt_conn, config, debug=False):
-        self._lock = threading.Lock()
+        self._lock = threading.RLock()
         self.conn = mqtt_conn
         self.interface = None
         self._state = self.set_state(SERVER_STATES.GESTURE_CAPTURE.value)
@@ -265,7 +265,7 @@ class ProfileSelectState(ServerState):
 
         except (KeyboardInterrupt, Exception) as e:
             print(e)
-            #exit(1)
+            exit(1)
 
     def on_button_press(self, interface, pressed):
         if pressed:
