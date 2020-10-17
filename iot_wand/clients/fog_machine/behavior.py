a7f3176d036cbb1eaee616e959784fbb696a602e
Sjshovan <SJSHOVAN@GMAIL.COM>
2020-10-17 00:31:01 -0400

    no message

--- iot_wand/clients/fog_machine/behavior.py
+++ iot_wand/clients/fog_machine/behavior.py
@@ -1,8 +1,60 @@
 from iot_wand import helpers as _h
 import settings as _s
+import time
+
+_h.restrict_client_system('Linux')
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+
+
+class FoggerManager():
+    def __init__(self):
+        self._state = False
+        self._on_pin = 23
+        self._off_pin = 24
+        self._t_delay = 2
+
+        #setup
+        GPIO.setup(self._on_pin, GPIO.OUT)
+        GPIO.setup(self._off_pin, GPIO.OUT)
+
+    def on(self):
+        self.state = True
+        GPIO.output(self._on_pin, GPIO.HIGH)
+        time.sleep(self._t_delay)
+        GPIO.cleanup()
+
+    def off(self):
+        self.state = False
+        GPIO.output(self._off_pin, GPIO.HIGH)
+        time.sleep(self._t_delay)
+        GPIO.cleanup()
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, value):
+        if isinstance(value, bool):
+            self.state = value
+
+    def toggle(self):
+        self.state = not self.state
+        f = self.off if self.state else self.on
+        f()
+
+fogger_manager = FoggerManager()
 
 def on_button(pressed):
-    pass
+    if pressed:
+        print('toggling fogger...')
+        fogger_manager.toggle()
+        print('fogger state is now %s' % str(fogger_manager.state))
+        
 
 def on_spell(gesture, spell):
     print(spell)
